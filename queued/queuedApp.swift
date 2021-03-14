64e72c7640e9caa04e7bec1254c96fd0170377a2
Ted <teddsbennett@gmail.com>
2021-03-14 12:09:08 +0000

    Initial Commit

---
+++ queued/queuedApp.swift
@@ -0,0 +1,17 @@
+//
+//  queuedApp.swift
+//  queued
+//
+//  Created by Ted Bennett on 14/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct queuedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
