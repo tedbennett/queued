d980c6d6aa4418bdd7adc6de8aca70364080551c
Ted <teddsbennett@gmail.com>
2021-08-07 10:11:58 +0100

    Add splash screen on app open

---
+++ queued/Views/SplashView.swift
@@ -0,0 +1,20 @@
+//
+//  SplashView.swift
+//  queued
+//
+//  Created by Ted Bennett on 07/08/2021.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        ProgressView()
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
--- queued/queuedApp.swift
+++ queued/queuedApp.swift
@@ -10,20 +10,33 @@ import Firebase
 
 @main
 struct queuedApp: App {
+    @State private var loading = true
     init() {
         FirebaseApp.configure()
-        UserManager.shared.checkUser()
+    }
+    
+    func joinSession(from url: URL) {
+        guard url.host == "www.kude.app" || url.host == "kude.app", url.pathComponents.count == 3, url.pathComponents[1] == "session" else {
+            return
+        }
+        SessionManager.shared.joinSession(id: url.pathComponents[2])
     }
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .onOpenURL { url in
-                    guard url.host == "www.kude.app" || url.host == "kude.app", url.pathComponents.count == 3, url.pathComponents[1] == "session" else {
-                        return
-                    }
-                    SessionManager.shared.joinSession(id: url.pathComponents[2])
+            Group {
+                switch loading {
+                    case true: SplashView()
+                    case false: ContentView()
                 }
+            }.onOpenURL { url in
+                joinSession(from: url)
+            }
+            .onAppear {
+                UserManager.shared.checkSession() { _ in
+                    self.loading = false
+                }
+            }
         }
     }
 }
